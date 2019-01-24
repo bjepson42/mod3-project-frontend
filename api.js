e81e02dc9cf38a00bd8d4c893bf4c948ca385fca
github username <github email address>
2019-01-23 22:00:53 -0500

    rework game flow

--- api.js
+++ api.js
@@ -11,6 +11,42 @@ function populateCompetingForDropdown(){
   });
 }
 
+function saveUsersToDb(user1name, user2name){
+  //data = {name: user1name},{name: user2name};
+  //FIX ME WITH CUSTOM ROUTE
+  data1 = {name: user1name};
+  data2 = {name: user2name};
+  postData(data1, "users");
+  postData(data2, "users");
+}
+
+function saveBetToDb(betName){
+  data1 = {bet: betName};
+  postData(data1, "bets");
+}
+
+function saveWordsToDb(word1name, word2name){
+  //data = {name: user1name},{name: user2name};
+  //FIX ME WITH CUSTOM ROUTE
+  data1 = {name: word1name};
+  data2 = {name: word2name};
+  postData(data1, "words");
+  postData(data2, "words");
+}
+
+function postData(data, route){
+  return fetch(`http://localhost:3000/${route}`,
+    {
+      method: "POST",
+      headers:
+      {
+        "content-type":"application/json",
+        accept: "application/json"
+      },
+      body: JSON.stringify(data)
+  })
+};
+
 function populateTeamDropdowns(){
   let allData = fetchData("users");
   allData.then(res => res.json())
@@ -29,9 +65,12 @@ function populateWord(){
   let allData = fetchData("words");
   allData.then(res => res.json())
   .then(json => {
-          let gameWord = document.getElementById("game-title");
-          let randomWord = json.data[Math.floor(Math.random()*json.data.length)];
-          gameWord.value = randomWord.attributes.name;
+          let gameWord1 = document.getElementById("game-1-word");
+          let gameWord2 = document.getElementById("game-2-word")
+          let randomWord1 = json.data[Math.floor(Math.random()*json.data.length)];
+          let randomWord2 = json.data[Math.floor(Math.random()*json.data.length)];
+          gameWord1.value = randomWord1.attributes.name;
+          gameWord2.value = randomWord2.attributes.name;
   });
 }
 
--- script.js
+++ script.js
@@ -1,24 +1,136 @@
 const lengthOfGame = 5; //length of game in seconds
 
-populateDropdowns();
-
+//populateDropdowns();
+document.getElementById("start-next-round-button").hidden = true;
 document.getElementById("submit-answer-button").hidden = true;
 document.getElementById("game-answer-div").hidden = true;
+document.getElementById("game-title-div").hidden = true;
 document.getElementById("start-button").addEventListener("click", function(){startGame()});
 document.getElementById("submit-answer-button").addEventListener("click", function(){checkCompleteGame()});
 
 
-function populateDropdowns(){
-  populateCompetingForDropdown();
+function startGame(event){
+  context.clearRect(0,0, canvas.width, canvas.height);
+  document.getElementById("start-button").remove();
+  paintTeams();
+}
+
+function paintTeams(){
+  let teamRowDiv = document.getElementById('team-row-div');
+
+  let teamRow = `<div class="col-md">
+    <h3>Team 1</h3>
+      <input type="text" name="user1" list="selectUser1" id="user-1-input"/>
+      <datalist id="selectUser1">
+      </datalist>
+    </div>
+    <div class="col-sm">
+      <h2>VS.</h2>
+    </div>
+    <div class="col-md">
+    <h3>Team 2</h3>
+      <input type="text" name="user2" list="selectUser2" id="user-2-input"/>
+      <datalist id="selectUser2">
+      </datalist>
+    </div>`;
+  teamRowDiv.innerHTML = teamRow;
+
+  let buttonDiv = document.getElementById('pick-your-punishment-button-div');
+  let button = `<button id="pick-your-punishment-button">Pick Your Punishment!</button>`;
+  buttonDiv.innerHTML = button;
+  buttonDiv.addEventListener("click", submitPickYourPunishment);
   populateTeamDropdowns();
-  populateWord();
 };
 
 
-function startGame(e){
-  context.clearRect(0,0, canvas.width, canvas.height);
-  preTimer(3);
+function submitPickYourPunishment(){
+  let team1Name = document.getElementById('user-1-input');
+  let team2Name = document.getElementById('user-2-input');
+  if (team1Name.value === team2Name.value || team1Name.value === "" || team2Name.value === ""){
+    alert("Select different team names!");
+  } else {
+    team1Name.disabled = true;
+    team2Name.disabled = true;
+    document.getElementById("pick-your-punishment-button").remove();
+    saveUsersToDb(team1Name.value, team2Name.value);
+    paintPickYourPunishment();
+  }
+};
+
+function paintPickYourPunishment(){
+  let punishmentDiv = document.getElementById('punishment-div');
+
+  let punishment = `<div id="punishment">
+    <h3>The Loser Will:</h3>
+    <input type="text" name="product" list="betName" id="bet-name-list"/>
+    <datalist id="betName">
+    </datalist>
+  </div>`;
+  punishmentDiv.innerHTML = punishment;
+  let buttonDiv = document.getElementById('pick-your-words-button-div');
+  let button = `<br><button id="pick-your-words-button">Pick Your Words!</button>`;
+  buttonDiv.innerHTML = button;
+  buttonDiv.addEventListener("click", submitPickYourWords);
+  populateCompetingForDropdown();
+};
+
+
+function submitPickYourWords(){
+  let betName = document.getElementById('bet-name-list');
+  if (betName.value === ""){
+    alert("Select a Punishment!");
+  } else {
+    betName.disabled = true;
+    document.getElementById("pick-your-words-button").remove();
+    saveBetToDb(betName.value);
+    paintPickYourWords();
+  }
+
+};
+
+function paintPickYourWords(){
+  let wordsRowDiv = document.getElementById('words-row-div');
+
+  let wordsRow = `<div class="col-md">
+            <input type="password" name="word1" list="selectWord1" id="game-1-word">
+            <input type="checkbox" onclick="toggleHidePassword('game-1-word')">
+            <datalist id="selectWord1">
+            </datalist>
+          </div>
+          <div class="col-sm">
+            <h2>Enter Your Words</h2>
+          </div>
+          <div class="col-md">
+            <input type="password" name="word2" list="selectWord2" id="game-2-word">
+            <input type="checkbox" onclick="toggleHidePassword('game-2-word')">
+            <datalist id="selectWord2">
+            </datalist>
+          </div>`;
+  wordsRowDiv.innerHTML = wordsRow;
+  let buttonDiv = document.getElementById('begin-game-button-div');
+  let button = `<br><button id="begin-game-button">Ready Team 1!</button>`;
+  buttonDiv.innerHTML = button;
+  buttonDiv.addEventListener("click", beginGame);
+  populateWord();
 }
+
+function beginGame(){
+  let game1Word = document.getElementById('game-1-word');
+  let game2Word = document.getElementById('game-2-word');
+  if (game1Word.value === game2Word.value || game1Word.value === "" || game2Word.value === ""){
+    alert("Select different team names!");
+  } else {
+    game1Word.disabled = true;
+    game2Word.disabled = true;
+    document.getElementById("begin-game-button").remove();
+    saveWordsToDb(game1Word.value, game2Word.value);
+    preTimer(3);
+    paintBeginGame();
+  }
+
+
+};
+
 
 function preTimer(seconds){
   let timeLeft = seconds;
@@ -28,7 +140,7 @@ function preTimer(seconds){
     if (timeLeft === 3) {
       timerDiv.innerHTML = "Ready";
       document.getElementById("game-answer").innerText = "";
-      document.getElementById("start-button").hidden = true;
+      document.getElementById("start-next-round-button").hidden = true;
       document.getElementById("submit-answer-button").hidden = false;
       document.getElementById("game-answer-div").hidden = false;
       document.getElementById("game-title-div").hidden = true;
@@ -75,10 +187,19 @@ function toggleHideTitleDiv() {
   }
 }
 
+function toggleHidePassword(elementId) {
+  var x = document.getElementById(elementId);
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
 
 function checkCompleteGame(){
   if (document.getElementById("game-answer").value != ""){
-    document.getElementById("start-button").hidden = false;
+    document.getElementById("start-next-round-button").hidden = false;
     document.getElementById("submit-answer-button").hidden = true;
     document.getElementById("game-answer-div").hidden = true;
     document.getElementById("game-title-div").hidden = false;
